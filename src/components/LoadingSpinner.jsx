e582f910f91df0a053615012cc47ec615bb917bb
DKB21 <88276913+DKB21@users.noreply.github.com>
2025-09-12 19:09:10 +0700

    update: change loading icon

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,8 +3,8 @@ import {Blend, Dice5, Dices, Dribbble, Hourglass, Loader, LoaderPinwheel} from "
 export default function LoadingSpinner({marginTop = true}) {
   return (
     <div className={`flex justify-center ${marginTop && "mt-8"} animate-bounce`}>
-      {/*<div className={"w-4 h-4 bg-main animate-spin"}/>*/}
-      <Hourglass strokeWidth={2.75} size={24} className={"text-main animate-spin"}/>
+      <div className={"w-4 h-4 bg-main animate-spin"}/>
+      {/*<Hourglass strokeWidth={2.75} size={24} className={"text-main animate-spin"}/>*/}
       {/*<Blend className={"text-main animate-spin"}/>*/}
     </div>
   )
